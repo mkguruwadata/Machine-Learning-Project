8a55f0e765205ebf9fae2155e9efa845b10a69a4
mkguruwadata <kellymuchangi97@gmail.com>
2024-09-05 20:46:27 +0300

    hyperparameter training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -51,7 +51,6 @@ class DataIngestion:
             raise CustomException(e, sys)
 
 if __name__ == "__main__":
-    try:
         # Data Ingestion
         obj = DataIngestion()
         train_data, test_data = obj.initiate_data_ingestion()
@@ -61,8 +60,6 @@ if __name__ == "__main__":
         train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
         # Model Training
-        model_trainer = ModelTrainer()
+        model_trainer=ModelTrainer()
         print(model_trainer.initiate_model_trainer(train_arr, test_arr))
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        raise CustomException(e, sys)
+   
